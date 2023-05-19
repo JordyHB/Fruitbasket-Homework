e0535975859352e147d24be81152f629b47674a6
jord1990 <jordy.hoogeboom@hotmail.nl>
2023-05-19 21:33:49 +0200

    Finshed a yank version

--- src/App.js
+++ src/App.js
@@ -5,35 +5,66 @@ import FruitProduct from "./Components/FruitProduct";
 
 function App() {
 
-    const [allCounts, setCount] = useState({
-        product1: 3,
-        product2: 0,
-        product3: 0,
-        product4: 0,
+    // an array to allow mapping over all products.
+    const fruitProducts = [
+        "Aardbeien",
+        "Bananen",
+        "Appels",
+        "Kiwi's"
+    ]
+    // creates an array of state objects with the initial count of 0 and the product name as key
+    const initialCounts = fruitProducts.map( (productName) => {
+        return { [productName]: 10 }
     })
+    // defines the state
+    const [allCounts, setCount] = useState(initialCounts)
 
-    function updateCount(productCount, product) {
-        console.log(productCount)
-        allCounts[product].setState(1)
 
+
+    function addToCount(index, product) {
+
+        console.log('ran')
+        console.log(index, product)
+        // states aren't mutable so a new object is made with the changes to the relevant product
+        setCount((prevState)  => {
+            const newState = {...prevState}
+            newState[index][product] = newState[index][product] + 1
+            return newState
+        })
 
     }
 
+    function subtractOfCount(index, product) {
+        console.log('ran')
+        console.log(index, product)
+        console.log(allCounts)
+        // states aren't mutable so a new object is made with the changes to the relevant product
+        setCount((prevState)  => {
+            const newState = {...prevState}
+            // subtracts 1 from the count of the relevant product by using the index and the product name
+            if (newState[index][product] >= 0) {
+                newState[index][product] = newState[index][product] - 1
+                return newState
+            }
+            // returns the previous state if the count is already 0 to prevent negative numbers
+            return newState
+        })
+    }
 
     return (
         <>
             <h1>Fruitmand bezorgservice</h1>
+            {fruitProducts.map( (productName, index) =>
             <FruitProduct
-                productName="Aardbeien"
-                setState={ setCount }
-                count={ allCounts.product1}
-
+                // key for React itself
+                key={index}
+                index={index}
+                productName={productName}
+                add={ addToCount }
+                currentCount={allCounts[index][productName]}
+                subtract={ subtractOfCount }
             />
-            <FruitProduct
-                productName="Bananen"
-            />
-        {/*    <button*/}
-        {/*        onClick={}>Reset</button>*/}
+            )}
         </>
     );
 }
--- src/Components/FruitProduct.js
+++ src/Components/FruitProduct.js
@@ -1,11 +1,7 @@
 import ChangeCountButton from "./ChangeCountButton";
 
-function FruitProduct({ productName, setState, count }) {
 
-    function handleUpdate() {
-        console.log(count)
-        return setState(5)
-    }
+function FruitProduct({ productName, add, subtract, currentCount, index}) {
 
     return (
         <div>
@@ -13,14 +9,14 @@ function FruitProduct({ productName, setState, count }) {
             <span>{ productName }</span>
             <ChangeCountButton
                 variant="-"
-                update={ () => { return setState( count + 1) }}
+                update={ () => subtract(index, productName) }
 
 
             />
-            <span> { count }</span>
+            <span> { currentCount }</span>
             <ChangeCountButton
                 variant="+"
-
+                update={ () => add(index, productName)}
             />
 
         </div>
